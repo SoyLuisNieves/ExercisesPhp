939ba86da99d85ea752d731233ce60ec920cf7d2
Luis Fernando Nieves Nieves <luis.fernan.nieves@gmail.com>
2019-10-06 18:55:16 -0500

    Use of variables

---
+++ 2.-Variables.php
@@ -0,0 +1,15 @@
+<?php 
+
+// Variables CamelCase
+$varInteger = 1;
+$varBoolean = true;
+$varFloat = 10.25;
+$varString = 'Hello World';
+
+// Variables Snake_Case
+$var_string = 'Good Bye!';
+
+// See data type
+var_dump($varInteger);
+
+ ?>
